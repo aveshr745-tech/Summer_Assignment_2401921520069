abd2825d8c606b8a36772663470e058805f2ca41
Avesh Maurya <aveshr745@gmail.com>
2026-06-02 07:40:57 +0530

    Implement maximum subarray solution in C++

---
+++ Week_01/Day_02/Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int maxi = INT_MIN;
+       int currSum =0;
+       for(int x : nums){
+        currSum += x;
+        maxi = max(maxi, currSum);
+        if(currSum <0){
+        currSum =0;
+       }
+       }
+        return maxi;
+    }
+};
