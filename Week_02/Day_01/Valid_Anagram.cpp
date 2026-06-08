60be7cda1c07c7be44c944a3524817937bf8d5a2
Avesh Maurya <aveshr745@gmail.com>
2026-06-08 06:41:41 +0530

    Add anagram checking methods using sorting and frequency
    Implemented two methods to check if two strings are anagrams: one using sorting and the other using a frequency array.

---
+++ Week_02/Day_01/Valid_Anagram.cpp
@@ -0,0 +1,40 @@
+// using sorting
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.size() != t.size()){
+        return false;
+      }
+      sort(s.begin(), s.end());
+      sort(t.begin(), t.end()); 
+      for(int i=0; i<s.size(); i++){
+          if(s[i] != t[i]){
+            return false;
+          }
+      }
+      return true; 
+    }
+};
+
+// using frequency array
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.size() != t.size()){
+        return false;
+      }
+      int freq[26] = {0};
+      for(int i=0; i<s.size(); i++) {
+        freq[s[i]- 'a']++;
+      }
+      for(int i=0; i<t.size(); i++){
+        freq[t[i]- 'a']--;
+      }
+      for(int i=0; i<26; i++){
+        if(freq[i] != 0) {
+            return false;
+        }
+      } 
+      return true;
+    }
+};
