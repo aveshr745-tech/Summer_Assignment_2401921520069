b3aecbdc45eb0ef37159446eb44d638112e7d49b
Avesh Maurya <aveshr745@gmail.com>
2026-06-15 08:55:28 +0530

    Add ReverseLinkedList implementation in C++
    Implemented two approaches to reverse a singly-linked list: using a stack and a recursive method.

---
+++ Week_03/Day_01/ReverseLinkedList.cpp
@@ -0,0 +1,54 @@
+// Using Stack
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        stack<int> st;
+        ListNode* temp = head;
+        while(temp != NULL) {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        while(!st.empty()) {
+            temp->val = st.top();
+            st.pop();
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
+// Recursive Approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       if(head == NULL || head->next == NULL) {
+        return head;
+       } 
+       ListNode* newHead = reverseList(head->next);
+       head->next->next = head;
+       head->next = NULL;
+       return newHead;
+    }
+};
