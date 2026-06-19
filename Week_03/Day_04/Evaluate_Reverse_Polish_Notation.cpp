1c7f51be322f6d14aaaa4817a19f6795cc1c5943
Avesh Maurya <aveshr745@gmail.com>
2026-06-19 08:01:46 +0530

    Add solution for evaluating RPN expressions
    Implement Evaluate Reverse Polish Notation using a stack.

---
+++ Week_03/Day_04/Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int solve(int a, int b, string token) {
+        if(token == "+") {
+            return a + b;
+        }
+        if(token == "-") {
+            return a - b;
+        }
+        if(token == "/") {
+            return a / b;
+        }
+        if(token == "*") {
+            return a * b;
+        }
+        return -1;
+    }
+    int evalRPN(vector<string>& tokens) {
+       stack<int> st;
+
+       for(string& token : tokens) {
+        if(token == "-" || token == "+" || token == "*" || token == "/") {
+
+            int b = st.top();
+            st.pop();
+
+            int a = st.top();
+            st.pop();
+
+            int res = solve(a, b, token);
+
+            st.push(res);
+        }
+        else{
+            st.push(stoi(token));
+        }
+       } 
+       return st.top();
+    }
+};
