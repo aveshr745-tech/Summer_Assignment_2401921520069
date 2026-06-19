42962defd7d736a397028f534e9d33c7a45b2221
Avesh Maurya <aveshr745@gmail.com>
2026-06-19 15:13:54 +0530

    Add LargestRectangleInHistogram.cpp implementation
    Implement largestRectangleArea function to calculate the maximum rectangular area in a histogram.

---
+++ Week_03/Day_04/LargestRectangleInHistogram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+      int n = heights.size();
+
+      stack<int> st;
+      vector<int> leftSmall(n), rightSmall(n);
+
+      for(int i = 0; i < n; i++) {
+        while(!st.empty() && heights[st.top()] >= heights[i]) {
+            st.pop();
+        }
+        leftSmall[i] = st.empty() ? 0 : st.top() + 1;
+
+        st.push(i);
+      }  
+      while(!st.empty()) st.pop();
+
+      for(int i = n-1; i >= 0 ; i--) {
+         while(!st.empty() && heights[st.top()] >= heights[i]) {
+            st.pop();
+         }
+         rightSmall[i] = st.empty() ? n-1 : st.top() - 1;
+
+         st.push(i);
+      }
+      int maxA = 0;
+      for(int i = 0; i < n; i++) {
+        int width = rightSmall[i] - leftSmall[i] + 1;
+        maxA = max(maxA, width * heights[i]);
+      }
+      return maxA;
+    }
+};
